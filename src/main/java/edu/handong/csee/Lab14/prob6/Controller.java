d584dcf8ab846263646f92b41bffb70dee4c6a73
leehyunji0715@gmail.com <leehyunji0715@172.17.156.181>
2018-05-09 22:12:25 +0900

    prob6 done

--- src/main/java/edu/handong/csee/Lab14/prob6/Controller.java
+++ src/main/java/edu/handong/csee/Lab14/prob6/Controller.java
@@ -1,32 +1,31 @@
 package edu.handong.csee.Lab14.prob6;
 import java.util.Scanner;//import java.util.Scanner
-public class Controller {
+public class Controller {//public modifier class named Controller
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//call main method
 		Scanner input = new Scanner(System.in);//instantiate Scanner class
-		String name, race;
-		int age = 0;
-		CustomID obj = new CustomID();
-		int i=0;
-		while(i<=2){
-		try {
-		System.out.print("Enter your name: ");
-		name = input.nextLine();
-		obj.recieveName(name);
-		System.out.print("Enter your age: ");
-		age = input.nextInt();
-		input.nextLine();
-		obj.recieveAge(age);
-		System.out.print("Enter your race: ");
-		race = input.nextLine();
-		obj.recieveRace(race);
+		String name, race;//call String class type named name and race
+		int age = 0;//call integer named age
+		CustomID obj = new CustomID();//instantiate CustomID class named obj
+		int i=0;//call integer named i
+		while(i<=2){//while i<=2
+			try {//try
+				System.out.print("Enter your name: ");//print out to enter user's name
+				name = input.nextLine();//input entered String to name 
+				obj.recieveName(name);//call obj's recieveName method given name value
+				System.out.print("Enter your age: ");//print out to enter user's age
+				age = input.nextInt();//input entered integer value to age
+				input.nextLine();//get enter after calling nextInt()
+				obj.recieveAge(age);//call obj's recieveAge method given age value
+				System.out.print("Enter your race: ");//print out to enter user's race
+				race = input.nextLine();//input entered value to race 
+				obj.recieveRace(race);//call obj's recieveRace method given race value
 			}
-			catch(Exception e){
-				System.out.println(e.getMessage());
-			continue;
+			catch(Exception e){//catch Exception
+				System.out.println(e.getMessage());//print out each error message
+				continue;//continue loop(start again)
 			}
+		i++;//increase i
 			}
-			}
+		}
 	}
-
-}
