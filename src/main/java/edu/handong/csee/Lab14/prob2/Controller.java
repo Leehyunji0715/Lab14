8cd3f22e28e33b479a80bfa568815887b8401663
Leehyunji0715 <leehyunji0715@gmail.com>
2018-06-02 20:04:51 +0900

    원데이원커

--- src/main/java/edu/handong/csee/Lab14/prob2/Controller.java
+++ src/main/java/edu/handong/csee/Lab14/prob2/Controller.java
@@ -7,6 +7,6 @@ public class Controller {//public modifier class named Controller
 		In_Exception except = new In_Exception();//instantiate In_Exception named except
 		except.error();//call method of except   
 		
-	}
+	}     
 
 }
